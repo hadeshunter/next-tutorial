a48c49b6b254286bbaa3d6b4630f17979da361d2
hmtri <hmtri1412@gmail.com>
2020-05-24 18:42:16 +0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import fetcher from "../libs/fetcher";
+import { GetServerSideProps, GetStaticProps } from "next";
+import useSWR from "swr";
+
+const URL_PRODUCT_LIST = "https://beta-api.phatthanhcafe.com/product/all";
+
+export default function Home({ initialData }) {
+  const { data } = useSWR(URL_PRODUCT_LIST, fetcher, { initialData });
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Trending Projects</h1>
+      <div>
+        {data
+          ? data.map((product) => (
+              <p key={product.id}>
+                <Link href='/product/[id]' as={`/product/${product.id}`}>
+                  {product.unit && product.code ? (
+                    <a>
+                      {product.name} . {product.unit} . {product.code}
+                    </a>
+                  ) : (
+                    <a>{product.name}</a>
+                  )}
+                </Link>
+              </p>
+            ))
+          : "loading..."}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await fetcher(URL_PRODUCT_LIST);
+  return { props: { initialData: data } };
+};
